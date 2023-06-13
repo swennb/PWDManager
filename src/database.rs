1edea6d9447a4885b9bebf3f15eaf20588daec2a
swennb <ownageswen@gmail.com>
2023-06-13 11:40:39 +0200

    Added function to add to database

--- src/database.rs
+++ src/database.rs
@@ -1,15 +1,16 @@
 
+use magic_crypt::generic_array::typenum::True;
 use rusqlite::{Connection, Result, params};
 #[derive(Debug)]
 struct PasswordDummyData {
-    id: String,
+    id: i64,
     name: String,
 
 }
 pub fn create_database(database_name: String) -> Result<()>{
 
     let passw = PasswordDummyData{
-        id: String::from("1"),
+        id: 1,
         name: String::from("Test")
     };
 
@@ -37,8 +38,42 @@ pub fn read_database(database_name: String) -> Result<()>{
     })?;
 
     for x in data_iter{
-        print!("Found {:?}", x);
+        print!("Found {:?}\n", x);
     }
 
+    Ok(())
+}
+
+pub fn add_to_database(database_name: String, password: String) -> Result<()>{
+    let mut latest_id = 0;
+    let mut Exists = false;
+    let static_db_name = database_name.clone()+".sqlite";
+    let conn = Connection::open(static_db_name)?;
+    let mut rec = conn.prepare("SELECT * FROM passwords")?;
+    let data_iter = rec.query_map([], |row| {
+        Ok(PasswordDummyData{
+            id: row.get(0)?,
+            name: row.get(1)?
+        })
+    })?;
+    for x in data_iter{
+        let y = x?;
+        latest_id =  y.id;
+        let name = y.name;
+        if name.clone() == password{
+            Exists = true;
+        }
+    }
+    print!("{:?}\n", latest_id);
+    let data = PasswordDummyData{
+        id: latest_id.clone() + 1,
+        name: password,
+    };
+    if Exists != true{
+        conn.execute("INSERT INTO passwords (id, name) values (?1, ?2)", (&data.id, &data.name)).unwrap();
+    }
+    else{
+        print!("Error: Name already exists\n")
+    }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use magic_crypt::{new_magic_crypt, MagicCryptTrait};
 use std::collections::HashMap;
 use std::fs;
 use std::io;
-
+use std::path::Path;
 mod database;
 
 fn encrypt(database_name: String) {
@@ -12,12 +12,23 @@ fn encrypt(database_name: String) {
 }
 
 fn main() {
-   let mut line = String::new();
-   println!("Enter database name:");
-   let input1 = io::stdin().read_line(&mut line);
-   let line = line.trim().to_string();
-   // Dit moet beter kunnen maar voor de time being een letterlijke copy;
-   let line_copy = line.clone();
-   database::create_database(line).unwrap();
-   database::read_database(line_copy).unwrap();
+    let mut exists:bool;
+    let mut line = String::new();
+    println!("Enter database name:");
+    let input1 = io::stdin().read_line(&mut line);
+    let line = line.trim().to_string();
+    // Dit moet beter kunnen maar voor de time being een letterlijke copy;
+    let line_copy: String = line.clone();
+    let line_copy2: String = line.clone();
+    // Check if line exists
+    let full_file: String = line_copy + ".sqlite";
+    exists = Path::new(&full_file).exists();
+    if exists != true{
+        database::create_database(line).unwrap();
+    }
+    database::read_database(line_copy2.clone()).unwrap();
+    let password_to_add: String = String::from("Yep Cock");
+    database::add_to_database(line_copy2.clone(), password_to_add.clone() ).unwrap();
+    database::read_database(line_copy2.clone()).unwrap();
+
 }
